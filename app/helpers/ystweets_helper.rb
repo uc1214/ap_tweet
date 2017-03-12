73c81a7a7c6cd4b30daf85b7b06b5b8ee41e83cb
Yushi Saijo <yushi.saijo@gmail.com>
2017-03-12 15:01:56 +0000

    編集画面で投稿できないエラーを解決

--- app/helpers/ystweets_helper.rb
+++ app/helpers/ystweets_helper.rb
@@ -1,6 +1,6 @@
 module YstweetsHelper
   def choose_new_or_edit
-    if action_name == 'new' || action_name == 'confirm' || action_name = 'index'
+    if action_name == 'new' || action_name == 'confirm' || action_name == 'index'
       confirm_ystweets_path
     elsif action_name == 'edit'
       ystweet_path
